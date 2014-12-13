dcd654c76dbc32cf4b4198ed5d0c0969d8c61d1c
cnc-general <davidcy@live.com>
2014-12-12 23:50:02 -0500

    Started Init method

--- main.asm
+++ main.asm
@@ -20,6 +20,10 @@ filledSquare BYTE "O",0
 endl BYTE 0dh,0ah,0
 
 currentRow DWORD 0
+currentColumn DWORD 0
+
+outerLoopAction DWORD 0
+innerLoopAction DWORD 0
 
 requestedRow DWORD ?
 requestedColumn DWORD ?
@@ -29,16 +33,43 @@ rows DWORD 64 DUP (0Fh)
 
 .code
 main PROC
-	mov rows+252, 0FFFFh
-	jmp plot
+	call init
+	call plot
 	exit
 main ENDP
+
+init PROC
+	mov outerLoopAction, OFFSET innerLoop
+	mov innerLoopAction, OFFSET fillPieces
+	call outerLoop
+	ret
+init ENDP
+
+fillPieces PROC
+	push eax
+	push ebx
+	push edx
+
+	mov eax, [currentRow]
+	mov requestedRow, eax
+	mov ebx, 2
+	div ebx
+	cmp eax, 1
+
+	pop edx
+	pop ebx
+	pop eax
+	ret
+fillPieces ENDP
 
 plot PROC
 	push edx
 	push eax
 
-	call rowLoop
+	mov outerLoopAction, OFFSET innerLoop
+	mov innerLoopAction, OFFSET printRedPiece
+
+	call outerLoop
 
 	pop eax
 	pop edx
@@ -46,45 +77,51 @@ plot PROC
 	ret
 plot ENDP
 
-rowLoop PROC
-	mov ecx, 8
-
+outerLoop PROC
+	push ecx
+	mov ecx, 0
+	
 	loop_start: 
-		call columnLoop
-		loop loop_start
+		call outerLoopAction
+		inc ecx
+		cmp ecx, 8
+		jnge loop_start
 
+	pop ecx
 	ret
-rowLoop ENDP
+outerLoop ENDP
 
-columnLoop PROC
+innerLoop PROC
 	mov currentRow,ecx
 	push ecx
 	push eax
 
 	mov ecx, 0
 	loop_start:
+		mov currentColumn, ecx
 		mov eax, currentRow
+
 		mov requestedRow, eax
 		mov requestedColumn, ecx
-		
-		call getSquare
 
-		mov eax, [requestedSquare]
-		mov eax, [eax]
+		call innerLoopAction
+		
+		;call getSquare
+
+		;mov eax, [requestedSquare]
+		;mov eax, [eax]
 
 		inc ecx
 		cmp ecx, 8
-
 	jnge loop_start
 
-	mov edx, OFFSET endl
-	call WriteString
+	call printNewLine
 
 	pop eax
 	pop ecx
 	
 	ret
-columnLoop ENDP
+innerLoop ENDP
 
 getSquare PROC
 	push eax
@@ -106,5 +143,92 @@ getSquare PROC
 
 	ret
 getSquare ENDP
+
+printEmptyWhite PROC
+	push eax
+	push edx
+
+	mov eax, whiteSquareColor
+	mov edx, OFFSET emptySquare
+
+	call SetTextColor
+	call WriteString
+		
+	mov eax, defaultColor
+	call SetTextColor
+
+	pop edx
+	pop eax
+	ret
+printEmptyWhite ENDP
+
+printEmptyRed PROC
+	push eax
+	push edx
+
+	mov eax, redSquareColor
+	mov edx, OFFSET emptySquare
+
+	call SetTextColor
+	call WriteString
+
+	mov eax, defaultColor
+	call SetTextColor
+
+	pop edx
+	pop eax
+	ret
+printEmptyRed ENDP
+
+printRedPiece PROC
+	push eax
+	push edx
+
+	mov eax, redPieceColor
+	mov edx, OFFSET filledSquare
+
+	call SetTextColor
+	call WriteString
+
+	mov eax, defaultColor
+	call SetTextColor
+
+	pop edx
+	pop eax
+	ret
+printRedPiece ENDP
+
+printBlackPiece PROC
+	push eax
+	push edx
+
+	mov eax, blackPieceColor
+	mov edx, OFFSET filledSquare
+
+	call SetTextColor
+	call WriteString
+
+	mov eax, defaultColor
+	call SetTextColor
+
+	pop edx
+	pop eax
+	ret
+printBlackPiece ENDP
+
+printNewLine PROC
+	push eax
+	push edx
+
+	mov edx, OFFSET endl
+	call WriteString
+
+	mov eax, defaultColor
+	call SetTextColor
+
+	pop edx
+	pop eax
+	ret
+printNewLine ENDP
 
 END main
