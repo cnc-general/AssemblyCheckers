2aeda84695831f06e11b1ba2b9f1aa3ccda128d1
cnc-general <davidcy@live.com>
2014-12-14 10:19:21 -0500

    Board Completed, Halfway through movement rules

--- main.asm
+++ main.asm
@@ -1,4 +1,3 @@
-
 TITLE MASM Template						(main.asm)
 
 ; Description:
@@ -6,31 +5,42 @@ TITLE MASM Template						(main.asm)
 ; Revision date:
 
 INCLUDE Irvine32.inc
+INCLUDE Data.inc
 .data
 
 Userinput DWORD 0
-X1 DWORD 0
-Y1 DWORD 0
-X2 DWORD 0
-Y2 DWORD 0
+R1 DWORD 0
+C1 DWORD 0
+R2 DWORD 0
+C2 DWORD 0
 
 Pieces DWORD 48 DUP (0)
+rows DWORD 64 DUP (0)
 
+;=======Strings======
+endl BYTE 0dh,0ah,0
 columnHeader BYTE " 01234567", 0
 rowHeader BYTE " ",0
+inputString BYTE 0dh, 0ah, "Please Enter Command as 4 digit number...", 0dh, 0ah, "Enter using format: SrcRow_SrcCol_DstRow_DstCol", 0dh,0ah, "Example: 0123", 0dh,0ah, "Command: ",0
+errorString BYTE 0dh, 0ah, "That is an invalid move. ",0
 
-defaultColor DWORD white+(black*16)
-
-redSquareColor DWORD black+(red*16)
-whiteSquareColor DWORD black+(white*16)
-redPieceColor DWORD red+(white*16)
-greenPieceColor DWORD green+(white*16)
+greenPlayerString BYTE 0dh, 0ah, "Green Player's Turn!",0dh, 0ah,0dh, 0ah,0
+redPlayerString BYTE 0dh, 0ah, "Red Player's Turn!",0dh, 0ah,0dh, 0ah,0
 
 emptySquare BYTE " ",0
 filledSquare BYTE "O",0
 kingSquare BYTE "K",0
+;====================
 
-endl BYTE 0dh,0ah,0
+;========Colors======
+defaultColor DWORD white+(black*16)
+redSquareColor DWORD black+(red*16)
+whiteSquareColor DWORD black+(white*16)
+redPieceColor DWORD red+(white*16)
+greenPieceColor DWORD green+(white*16)
+greenPlayerColor DWORD green+(black*16)
+redPlayerColor DWORD red+(black*16)
+;====================
 
 currentRow DWORD 0
 currentColumn DWORD 0
@@ -46,29 +56,353 @@ requestedPiece DWORD 0
 requestedPieceID DWORD 0
 requestedPieceStatus DWORD 0
 
-rows DWORD 64 DUP (0)
-
 CreatePieceCount DWORD 0
-
 RedPieceCount DWORD 0
 GreenPieceCount DWORD 0
+
+InputErrorOccured DWORD 0
+MoveIsJump DWORD 0
+CurrentPlayerID DWORD 0
 
 .code
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Start of main
 
 main PROC
-	;call init
-	;call input
+	call InitCheckers
 
+	start:
+		call GetCommand
+		call PerformCommand
+		call CheckIfError
+		call EndIfWon
+
+		call ReplotBoard
+		jmp start
+
+main ENDP
+
+SwapPlayers PROC
+	push eax
+		mov eax, CurrentPlayerID
+		XOR eax, 000000001h
+		mov CurrentPlayerID, eax
+	pop eax
+	ret
+SwapPlayers ENDP
+
+CheckIfError PROC
+	push eax
+
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je error_occured
+	jmp no_error
+
+	error_occured:
+		push edx
+		lea edx, errorString
+		call WriteString
+		pop edx
+
+		call WaitMsg
+
+		call printNewLine
+		jmp end_check
+
+		no_error:
+			call SwapPlayers
+			jmp end_check
+	end_check:
+		mov InputErrorOccured, 0
+		pop eax
+		ret
+CheckIfError ENDP
+
+MovePiece PROC
+	push eax
+	push ebx
+	push ecx
+
+	mov eax, R1
+	mov ebx, C1
+
+	mov requestedRow, eax
+	mov requestedColumn, ebx
+	call GetRequestedSquare
+	
+	mov eax, [requestedSquare]
+	mov ecx, DWORD PTR [eax]
+	mov DWORD PTR [eax], 0
+
+	mov eax, R2
+	mov ebx, C2
+
+	mov requestedRow, eax
+	mov requestedColumn, ebx
+	call GetRequestedSquare
+
+	mov eax, [requestedSquare]
+	;mov eax, [eax]
+	mov DWORD PTR [eax], ecx
+
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+MovePiece ENDP
+
+PerformCommand PROC
+	
+	call CheckDiagonal
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je end_perform
+
+	call CheckAdjacent
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je end_perform
+
+	call CheckSource
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je end_perform
+
+	call CheckDirection
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je end_perform
+
+	call CheckDestination
+	mov eax, InputErrorOccured
+	cmp eax, 1
+		je end_perform
+
+	call MovePiece
+
+	end_perform:
+		ret
+PerformCommand ENDP
+
+CheckDiagonal PROC
+	push eax
+	push ebx
+	push ecx
+	push edx
+
+	mov eax, R1
+	mov ebx, C1
+	mov ecx, R2
+	mov edx, C2
+
+	cmp eax, ecx
+		je error_occured
+
+	cmp ebx, edx
+		je error_occured
+
+	jmp end_check
+
+	error_occured:
+		mov InputErrorOccured, 1
+		jmp end_check
+
+	end_check:
+		pop edx
+		pop ecx
+		pop ebx
+		pop eax
+		ret
+CheckDiagonal ENDP
+
+CheckAdjacent PROC
+	push eax
+	push ebx
+	push ecx
+	push edx
+
+	mov eax, R1
+	mov ebx, C1
+	mov ecx, R2
+	mov edx, C2
+
+	sub eax, ecx
+	cmp eax, 1
+		jg error_occured
+
+	sub ebx, edx
+	cmp ebx, 1
+		jg error_occured
+
+	jmp end_check
+
+	error_occured:
+		mov InputErrorOccured, 1
+		jmp end_check
+	
+	end_check:
+		pop edx
+		pop ecx
+		pop ebx
+		pop eax
+		ret
+CheckAdjacent ENDP
+
+CheckSource PROC
+	push eax
+	push ebx
+
+	mov eax, R1
+	mov ebx, C1
+
+	mov requestedRow, eax
+	mov requestedColumn, ebx
+	call GetRequestedSquare
+
+	mov eax, [requestedSquare]
+	mov eax, [eax]
+	cmp eax, 0
+		je error_occured
+
+	mov requestedPiece, eax
+	call GetRequestedPiece
+	
+	mov eax, requestedPieceID
+	mov ebx, CurrentPlayerID
+	cmp eax, ebx
+		jne error_occured
+
+	jmp end_check
+
+	error_occured:
+		mov InputErrorOccured, 1
+		jmp end_check
+
+	end_check:
+		pop ebx
+		pop eax
+		ret
+CheckSource ENDP
+
+CheckDirection Proc
+	push eax
+	push ebx
+
+	mov eax, requestedPieceStatus
+	cmp eax, 2
+		je end_check
+
+	mov eax, CurrentPlayerID
+	cmp eax, 0
+		je player_0
+	jmp player_1
+
+	player_0:
+		mov eax, R1
+		mov ebx, R2
+		cmp eax, ebx
+			jge error_occured
+		jmp end_check
+
+	player_1:
+		mov eax, R1
+		mov ebx, R2
+		cmp eax, ebx
+			jle error_occured
+		jmp end_check
+
+	error_occured:
+		mov InputErrorOccured, 1
+		jmp end_check
+
+	end_check:
+		pop ebx
+		pop eax
+		ret
+CheckDirection ENDP
+
+CheckDestination PROC
+	push eax
+	push ebx
+
+	mov eax, R2
+	mov ebx, C2
+
+	mov requestedRow, eax
+	mov requestedColumn, ebx
+	call GetRequestedSquare
+
+	mov eax, [requestedSquare]
+	mov eax, [eax]
+	cmp eax, 0
+		je move_standard
+
+	mov requestedPiece, eax
+	call GetRequestedPiece
+	
+	mov eax, requestedPieceID
+	mov ebx, CurrentPlayerID
+	cmp eax, ebx
+		je error_occured
+	
+	jmp move_jump
+
+
+	move_jump:
+		mov MoveIsJump, 1
+		jmp end_check
+
+
+	move_standard:
+		mov MoveIsJump, 0
+		jmp end_check
+
+	error_occured:
+		mov InputErrorOccured, 1
+		jmp end_check
+
+	end_check:
+		pop ebx
+		pop eax
+		ret
+CheckDestination ENDP
+
+EndIfWon PROC
+	push eax
+
+	call GetPieceCounts
+	
+	mov eax, RedPieceCount
+
+			;DEBUG;
+				;mov eax, 0
+
+	cmp eax, 0
+	je end_program
+
+	mov eax, GreenPieceCount
+	cmp eax, 0
+	je end_program
+
+	jmp return_main
+	
+	end_program:
+		exit
+
+	return_main:
+		pop eax
+		ret
+
+EndIfWon ENDP
+
+InitCheckers PROC
+	
 	call CreatePieces
 	call InitBoard
-
 	call ReplotBoard
-
-	;call GetPieceCounts
-	exit
-main ENDP
+	ret
+InitCheckers ENDP
 
 InitBoard PROC
 	mov innerLoopAction, OFFSET PlacePiece
@@ -138,7 +472,7 @@ AddPieceToBoard PROC
 	mov ebx, [currentColumn]
 	mov requestedColumn, ebx
 
-	call GetSquare
+	call GetRequestedSquare
 
 	cmp eax, 2
 	jle piece
@@ -161,9 +495,6 @@ AddPieceToBoard PROC
 
 		mov DWORD PTR [eax], ebx
 
-		mov eax, [requestedSquare]
-		mov eax, [eax]
-
 		mov eax, CreatePieceCount
 		inc eax
 		mov CreatePieceCount, eax
@@ -175,43 +506,62 @@ AddPieceToBoard PROC
 		ret
 AddPieceToBoard ENDP
 
-input proc
+GetCommand PROC
+	call PromptCommand
+	call ReadCommand
+	ret
+GetCommand ENDP
+
+PromptCommand PROC
+	push edx
+		lea edx, inputString
+		call WriteString
+	pop edx
+	ret
+PromptCommand ENDP
+
+ReadCommand PROC
 	push eax
 	push ebx
 	push edx
 
 	call readint
+	
+	cmp eax, 7777
+	jg end_read
 
 	XOR edx, edx
 
 	mov Userinput,eax
 	mov ebx, 1000
 	div ebx
-	mov X1, eax
+	mov R1, eax
 	mov eax, edx
 
 	XOR edx, edx
 
 	mov ebx, 100
 	div ebx
-	mov Y1, eax
+	mov C1, eax
 	mov eax, edx 
 
 	XOR edx, edx
 
 	mov ebx, 10
 	div ebx
-	mov X2, eax
+	mov R2, eax
 	mov eax, edx
 
-	mov Y2, edx 
+	mov C2, edx 
 
-	pop edx
-	pop ebx
-	pop eax
 
-	ret
-input endp
+	end_read:
+		pop edx
+		pop ebx
+		pop eax
+
+		ret
+ReadCommand ENDP
 
 CreatePieces PROC
 	call PiecesOuterLoop
@@ -265,12 +615,12 @@ CreatePiece PROC
 	imul edx, 8
 
 	add edx, eax
-	lea eax,Pieces
+	lea eax, Pieces
 	add eax, edx
 
 	mov dword ptr[eax], ebx
 	add eax, 4
-	mov dword ptr[eax], 01h
+	mov dword ptr[eax], 02h
 
 	pop edx
 	pop ecx
@@ -369,13 +719,15 @@ ReplotBoard PROC
 	push edx
 	push eax
 
+	call Clrscr
+	call PrintTurnString
+
 	lea edx, columnHeader
 	call WriteString
 	call printNewLine
 
 	mov outerLoopAction, OFFSET ReplotBoardOuterAction
 	mov innerLoopAction, OFFSET	CreateSquare
-
 	call outerLoop
 
 	pop eax
@@ -383,6 +735,35 @@ ReplotBoard PROC
 
 	ret
 ReplotBoard ENDP
+
+PrintTurnString PROC
+	push eax
+	push edx
+	
+	mov eax, CurrentPlayerID
+	cmp eax, 0
+		je red_player
+	jmp green_player
+
+		red_player:
+			lea edx, redPlayerString
+			mov eax, redPlayerColor
+			jmp end_print
+
+		green_player:
+			lea edx, greenPlayerString
+			mov eax, greenPlayerColor
+			jmp end_print
+
+	end_print:
+		call SetTextColor
+		call WriteString
+		mov eax, defaultColor
+		call SetTextColor
+		pop edx
+		pop eax
+		ret
+PrintTurnString ENDP
 
 ReplotBoardOuterAction PROC
 	push eax
@@ -495,7 +876,7 @@ CreateSquare ENDP
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-GetSquare PROC
+GetRequestedSquare PROC
 	push eax
 	push ebx
 
@@ -514,7 +895,7 @@ GetSquare PROC
 	pop eax
 
 	ret
-GetSquare ENDP
+GetRequestedSquare ENDP
 
 ActiveSquare PROC
 	push eax
@@ -525,7 +906,7 @@ ActiveSquare PROC
 
 	mov requestedRow, eax
 	mov requestedColumn, ecx
-	call GetSquare
+	call GetRequestedSquare
 	
 	mov eax, [requestedSquare]
 	mov eax, [eax]
@@ -564,7 +945,7 @@ ActiveSquare PROC
 	is_green:
 		mov eax, requestedPieceStatus
 		cmp eax, 2
-		je green_piece
+		je green_king
 		jmp green_piece
 
 	green_piece:
@@ -691,7 +1072,7 @@ printGreenKing PROC
 	push edx
 
 	mov eax, greenPieceColor
-	lea edx, filledSquare
+	lea edx, kingSquare
 
 	call SetTextColor
 	call WriteString
